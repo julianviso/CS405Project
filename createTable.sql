2df5d3632b07ddb79eef58e6af938763d35bcd3a
Bryan Potere <bpotere@users.noreply.github.com>
2015-03-24 13:51:52 -0400

    Update createTable.sql

--- createTable.sql
+++ createTable.sql
@@ -39,10 +39,12 @@ CREATE TABLE Orders(order_id INTEGER NOT NULL AUTO_INCREMENT,
 					
 					
 CREATE TABLE Promotions(promo_id INTEGER NOT NULL AUTO_INCREMENT,
+					prod_id NOT NULL,
 					discount FLOAT NOT NULL,
 					startDate DATE NOT NULL,
 					endDate DATE,
 					managerOrdered INTEGER NOT NULL,
 					PRIMARY KEY (promo_id),
 					FOREIGN KEY (managerOrdered) REFERENCES Staff(sid),
+					FOREIGN KEY (prod_id) REFERENCES Products(prod_id),
 						CHECK (discount < 1));		
