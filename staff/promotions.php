9d124e9fba0a88e833a0551644753792d32ae198
Julian <julian@localhost.localdomain>
2015-04-12 22:14:53 -0400

    promotions in progress

---
+++ staff/promotions.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML>
+<head>
+<title>Staff/Employee Page</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="../css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<link href="../css/slider.css" rel="stylesheet" type="text/css" media="all"/>
+<link href="../css/menu.css" rel="stylesheet" type="text/css" media="all"/>
+<link href='http://fonts.googleapis.com/css?family=Doppio+One' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Monda' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="../js/jquery-1.7.2.min.js"></script>
+<script type="text/javascript" src="../js/move-top.js"></script>
+<script type="text/javascript" src="../js/easing.js"></script> 
+<script type="text/javascript" src="../js/nav.js"></script>
+<script type="text/javascript" src="../js/nav-hover.js"></script>
+<script type="text/javascript">
+  $(document).ready(function($){
+    $('#dc_mega-menu-orange').dcMegaMenu({rowItems:'4',speed:'fast',effect:'fade'});
+  });
+</script>
+    
+        <?php
+            session_start();
+            require "../sql/serverinfo.php";
+            if($_SESSION["manager"] == 1){
+                //if employee is a manager
+                include "../components/managerHeader_menu.html";
+            }
+            else{
+                //keep temporarily, delete after
+			  include "../components/staffHeader_menu.html";
+               //exit(); 
+            }
+		?>
+	<?php
+		$require "../sql/serverinfo.php";
+		$query = "SELECT *
+				FROMT Products";
+		$link = mysqli_connect($conn, $login, $password, $dbname);
+		$result = mysqli_query($link, $query);
+		if ($result){
+			echo '<tr><td align="center"><b>Name</b></td>';	
+		}
+	?>
+    
+</head>
+<body>
+
+</body>
+</html>
