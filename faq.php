9e9ab121aab8abf276914663e889d261438c6afc
julianviso <julian.viso@outlook.com>
2015-04-17 00:08:40 -0400

    faq page now has some useful information

--- faq.php
+++ faq.php
@@ -38,44 +38,44 @@ License URL: http://creativecommons.org/licenses/by/3.0/
     <div class="content">
     	<h2>Frequently Asked Questions</h2>
     	<div class="questions">
-				<h4>1.What is Lorem Ipsum?</h4>
-	        	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+				<h4>1.What is our company about?</h4>
+	        	<p>Ask Bryan.</p>
 	        </div>  
 	        <div class="questions">
-				<h4>2.What is Lorem Ipsum Lorem Ipsum has been the industry's standard dummy text?</h4>
-	        	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+				<h4>2.Why ask Bryan?</h4>
+	        	<p>Julian is too lazy to answer questions himself</p>
 	        </div>   
 	        <div class="questions">
-				<h4>3.What is Lorem Ipsum Lorem Ipsum has been the industry's?</h4>
-	        	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+				<h4>3. How can I purchase ___ item from your website?</h4>
+	        	<p>Simply Julian all your money and he'll gladly send you a picture of whatever item you wanted to actually purchase.</p>
 	        </div>   
 	        <div class="questions">
-				<h4>4.What is Lorem Ipsum dummy text of the printing?</h4>
-	        	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+				<h4>4.Can I be staff?</h4>
+	        	<p>No.</p>
 	        </div>   
 	        <div class="questions">
-				<h4>5.What is Lorem Ipsum printing and typesetting industry?</h4>
-	        	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+				<h4>5.Can I be manager?</h4>
+	        	<p>Yes.</p>
 	        </div>   
 	        <div class="questions">
-				<h4>6.What is Lorem Ipsum text of the printing?</h4>
-	        	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+				<h4>6. What is your best product?</h4>
+	        	<p>Bryan sells a copy of Ubuntu 14.04 for only 99 monthly payments of $19.99</p>
 	        </div>   
 	        <div class="questions">
 				<h4>7.What is Lorem Ipsum?</h4>
-	        	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+	        	<p>I have no idea.</p>
 	        </div>   
 	        <div class="questions">
-				<h4>8.What is Lorem Ipsum dummy text ever since the 1500s?</h4>
-	        	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+				<h4>8. How have you guys stayed in business for more than a day?</h4>
+	        	<p>Professor Qian threatens us with bad grades if we don't do his project.</p>
 	        </div>   
 	        <div class="questions">
-				<h4>9.What is Lorem Ipsum  Lorem Ipsum has been the industry's standard dummy text?</h4>
-	        	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+				<h4>9. Where is the repo for this stuff located?</h4>
+	        	<p>github.com/julianviso/CS405Project</p>
 	        </div>   
 	        <div class="questions">
-				<h4>10.What is Lorem Ipsum typesetting industry?</h4>
-	        	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+				<h4>10. How do I get coupons?</h4>
+	        	<p>Ask Julian to ask Bryan for one.</p>
 	        </div>    	
     </div>
  </div>
