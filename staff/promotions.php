7c547c958e58e38206d8ead486d81ac54a687714
JulianViso <julian.viso@outlook.com>
2015-04-17 15:19:01 -0400

    promotions now has function that should allow when table to get updated

--- staff/promotions.php
+++ staff/promotions.php
@@ -40,7 +40,71 @@
 <body>
 	
 	<?php
-		//some bug in here
+		 function addPromotion(){
+            global $conn, $login, $password, $dbname;
+            //Connect to the database.
+            $err_message = null;
+            $link = mysqli_connect($conn, $login, $password, $dbname);
+            if (mysqli_error($link)){
+                $err_message = mysqli_errno($link) . ": " . mysqli_error($link) . "\n";
+                return $err_message;
+            }
+            $discount = $_POST["discount"];
+            $startDate = $_POST["startDate"];
+            $endDate = $_POST["endDate"];
+            //Update the quantity
+            $query = "INSERT INTO Promotions VALUES
+                      (DEFAULT, '$discount', '$startDate', '$endDate', DEFAULT, DEFAULT)";
+            $result = mysqli_query($link, $query);
+            if (mysqli_error($link)){
+                $err_message = mysqli_errno($link) . ": " . mysqli_error($link) . "\n";
+            }
+            
+            $result = mysqli_query($link, "
+                        SELECT AUTO_INCREMENT 
+                        FROM information_schema.tables
+                        WHERE table_name = 'Promotions' 
+                        AND table_schema = DATABASE()
+                    ");
+            $data = mysqli_fetch_array($result);
+            //Now give thisOrder_id the next num
+            $thisOrder_id = $data["AUTO_INCREMENT"];
+            
+            return $thisOrder_id - 1;
+            
+        }
+			/*
+	       function updatePromotions(){
+            global $conn, $login, $password, $dbname;
+            //Connect to the database.
+            $err_message = null;
+            $link = mysqli_connect($conn, $login, $password, $dbname);
+            if (mysqli_error($link)){
+                $err_message = mysqli_errno($link) . ": " . mysqli_error($link) . "\n";
+                return $err_message;
+            }
+		  
+
+            //Discount
+            $query = 'UPDATE Promotions
+                      SET discount='.$_POST["discount"].'
+                      WHERE promo_id='.$_POST["promo_id"];
+            $result = mysqli_query($link, $query);
+            if (mysqli_error($link)){
+                    $err_message = mysqli_errno($link) . ": " . mysqli_error($link) . "\n";
+            }
+
+            $row_nums = mysqli_num_rows($result);
+            if ($row_nums < 1){
+                $err_message = 'Error: There is no promo with ID number: '.$item["promo_id"].'<br/>';
+            }
+
+            mysqli_close($link);
+            return $err_message;
+        }
+	   */
+
+
 		require "../sql/serverinfo.php";
 		$query = "SELECT *	FROM Products";
 		$link = mysqli_connect($conn, $login, $password, $dbname);
@@ -65,18 +129,21 @@
 				
 				//Get the input and button for updating sale price
 				echo '<form id=form'.$row['prod_id'].' method="post" action="promotions.php">
+				
 					<input type="hidden" name="prod_id" id="prod_id" value="'.$row['prod_id'].'"/>
-					<input type="text" name="changePrice" id="changePrice"/>
+					<input type="text" name="discount" id="discount"/>
 					<input type="date" name="startDate"/>
 					<input type="date" name="endDate"/>
 					<input type="submit" name="discount" id="discount" value="Commit"/>
 				</form></td>';
-				//</tr>
+				echo '</tr>';
 			}
 		}
 		else{
 			echo "some kind of error happened";
 		}
+
+	
 	?>
 	
 	
